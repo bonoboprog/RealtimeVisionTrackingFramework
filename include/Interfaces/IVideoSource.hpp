5ccc75d1f37689903080b8bc28ea4337f5615dfb
paolo11800@gmail.com <64153792+bonoboprog@users.noreply.github.com>
2026-05-11 14:16:27 +0200

    made cmake build to link include folder correctly

---
+++ include/Interfaces/IVideoSource.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <optional>
+
+/**
+ * Interface for any video source (Webcam, File, RTSP Stream).
+ */
+
+ class IVideoSource
--- include/Modules/CameraSource.hpp
+++ include/Modules/CameraSource.hpp
@@ -1,7 +1,2 @@
 #pragma once
-#include <opencv2/opencv.hpp>
-#include <optional>
-
-/**
- * Interfaccia per qualsiasi sorgente video (Webcam, File, Stream RTSP).
- */
+#include "Interfaces/IVideoSource.hpp"
