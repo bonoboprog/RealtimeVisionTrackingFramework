257f6353c3cc87d64d6a6c7c46f09b0fd554f23f
paolo11800@gmail.com <64153792+bonoboprog@users.noreply.github.com>
2026-05-12 10:24:39 +0200

    main updated

--- include/Core/FrameContext.hpp
+++ include/Core/FrameContext.hpp
@@ -5,7 +5,7 @@
 
 struct FrameContext {
     cv::Mat frame; // Current video frame
-    uint64_t framenumber = 0; // Frame number, incremented for each new frame captured
+    uint64_t frameNumber = 0; // Frame number, incremented for each new frame captured
     std::chrono::steady_clock::time_point timestamp;    // Time point when the frame was captured, 
                                                         // using steady_clock to avoid issues with system clock changes
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,37 @@
 #include <memory>
 #include "Modules/CameraSource.hpp"
 #include "Core/FrameContext.hpp"
+
+int main() {
+
+    // 1. Initialize the video source (CameraSource in this case)
+    std::unique_ptr<IVideoSource> src = std::make_unique<CameraSource>(0);
+
+    if (!src->isOpened()) {
+        std::cerr << "Error: Could not open video source!" << std::endl;
+        return -1;
+    }
+
+    FrameContext ctx;
+    std::cout << "Framework initializated. Press ESC to exit." << std::endl;
+
+    // 2. Main loop to capture and display frames (real-time acquisition)
+    while (true) {
+        auto frameOpt = src->getNextFrame();
+
+        if (!frameOpt.has_value()) break; // Exit loop if no frame is available (e.g., camera disconnected)
+
+        // Update the context
+        ctx.frame = frameOpt.value();
+        ctx.frameNumber++;
+        ctx.timestamp = std::chrono::steady_clock::now();
+
+        // Process the frame (for now, we just display it)
+        cv::imshow("Camera Feed", ctx.frame);
+        
+        // handle user input to exit the loop
+        if (cv::waitKey(1) == 27) break;
+    }
+
+    return 0;
+};
